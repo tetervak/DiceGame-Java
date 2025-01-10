19e2872b9c8c9876c2d1708890f870a8f0db972a
Alex Tetervak <tetervak@sheridancollege.ca>
2025-01-10 14:36:25 -0500

    init dice in setUp

--- src/test/java/ca/tetervak/dicegame/model/DiceTest.java
+++ src/test/java/ca/tetervak/dicegame/model/DiceTest.java
@@ -9,9 +9,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DiceTest {
 
+    Dice dice;
+
     @BeforeEach
     void setUp() {
         out.println("Starting test");
+        dice = new Dice();
     }
 
     @AfterEach
@@ -22,14 +25,12 @@ class DiceTest {
     @ValueSource(ints = {0,7})
     @DisplayName("check set illegal value")
     void setValue_illegalValue(int value) {
-        Dice dice = new Dice();
         assertThrows(IllegalArgumentException.class, () -> dice.setValue(value));
     }
 
     @Test
     @DisplayName("check set legal value")
     void setValue_legalValue() {
-        Dice dice = new Dice();
         dice.setValue(5);
         assertEquals(5, dice.getValue());
         assertThrows(IllegalArgumentException.class, () -> dice.setValue(0));
@@ -39,7 +40,6 @@ class DiceTest {
     @DisplayName("check range after roll")
     @RepeatedTest(2)
     void roll() {
-        Dice dice = new Dice();
         for (int i = 1; i <= 10; i++) {
             dice.roll();
             assertTrue(dice.getValue() <= 6);
